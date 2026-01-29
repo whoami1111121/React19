ecd24c70afb3a38b003f05723a0c80ebee3a1cff
whoami1111121 <udud0099@gmail.com>
2026-01-29 10:12:17 +0545

    update

---
+++ form-validation-class/zod/src/App.tsx
@@ -0,0 +1,17 @@
+ import './App.css'
+import ZodForm from './components/ZodForm'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>ZodForm </h1>
+      <br />
+      <div>
+        <ZodForm />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ form-validation-class/zod/src/components/ZodForm.tsx
@@ -0,0 +1,16 @@
+ import {z} from 'zod'
+
+
+ const userSchema = z.object({
+    name: z.string()
+ })
+const ZodForm = () => {
+  return (
+    <div>
+      ZodForm
+ZodForm
+    </div>
+  )
+}
+
+export default ZodForm
