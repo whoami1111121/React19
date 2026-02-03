316c877a435fcb972f04d41cc9e0252b00bc76fb
whoami1111121 <udud0099@gmail.com>
2026-02-03 09:45:42 +0545

    globlel > useContext start

--- globalstate/useContext/useContextClass/src/App.tsx
+++ globalstate/useContext/useContextClass/src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import TaskBase from "./components/TaskBase";
+export interface User {
+  name: string;
+  email: string;
+}
 
 function App() {
+  const user = useState<User>({
+    name: "UI",
+    email: "ui@gmail.com",
+  });
   return (
     <>
       <div className="bg-black text-white w-full min-h-screen text-2xl">
